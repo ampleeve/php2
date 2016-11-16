eb17a8e98280a58ce2f74b845d4b6c35ffdf52ca
ampleeve <e@ampleev.com>
2016-11-17 00:39:48 +0300

    ываыва

--- www/index.php
+++ www/index.php
@@ -1,3 +1,17 @@
 <?php
+abstract class MyAbstractClass {
 
-?>
+    abstract protected function getValue();
+    public function printValue() {
+
+        print $this->getValue() . "\n";
+    }
+}
+class ChildClass extends MyAbstractClass {
+
+    protected function getValue() {
+        return " ChildClass ";
+    }
+}
+$class1 = new ChildClass();
+$class1->printValue();
