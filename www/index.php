9ed1f04579d201d9ffc9893de63b3f7c9051b5ab
ampleeve <e@ampleev.com>
2016-11-17 14:11:02 +0300

    ыва

--- www/index.php
+++ www/index.php
@@ -1,14 +1,12 @@
 <?php
-abstract class Product {
-
-    public $id;
+/*abstract class Product {
     
     abstract protected function getValue();
     public function printValue() {
 
         print $this->getValue() . "\n";
     }
-}
+}*/
 
 
 abstract class Product{
@@ -49,26 +47,61 @@ abstract class Product{
 
     public function view(){
 
-        $this->showProduct();
-       /* echo "<h1>$this->name</h1>";
+        echo "<h1>$this->name</h1>";
         echo "Изображения: ";
-        foreach ($this->img as $img){
+        foreach ($this->img as $img) {
             echo "<p>$img</p>";
         }
         echo "<p>Цена: $this->price</p>";
         echo "<p>Описание товара: $this->description</p>";
         echo "Ключевые слова: ";
-        foreach ($this->keywords as $aKeyword){
+        foreach ($this->keywords as $aKeyword) {
             echo "<p>$aKeyword</p>";
-        }*/
+        }
+        $this->showProduct();
     }
 
 }
-class ChildClass extends MyAbstractClass {
+class Toy extends Product {
+
+    /**
+     * @var $age int - Ограничение по возрасту
+     * @var $type string - Тип игрушки
+     */
+    public $age;
+    public $type;
+
+    /**
+     * Toy constructor.
+     * @param $id
+     * @param $name
+     * @param $img
+     * @param $price
+     * @param $description
+     * @param $keywords
+     * @param $age
+     * @param $type
+     */
+    public function __construct($id, $name, $img, $price, $description, $keywords, $age, $type){
+        parent::__construct($id, $name, $img, $price, $description, $keywords);
+        $this->age = $age;
+        $this->type = $type;
+    }
+
+    protected function showProduct(){
+       /* parent::view();
+        echo "<p>Ограничение по возрасту(лет): $this->age</p>";
+        echo "<p>Тип игрушки: $this->type</p>";*/
+
+    }
+
+
+}
+/*class ChildClass extends MyAbstractClass {
 
     protected function getValue() {
         return " ChildClass ";
     }
 }
 $class1 = new ChildClass();
-$class1->printValue();
+$class1->printValue();*/
