2960b45d3baad704a3d0ed5db0de6379e8dc7fd8
ampleeve <e@ampleev.com>
2016-11-17 13:56:19 +0300

    sfsdf

--- www/index.php
+++ www/index.php
@@ -9,6 +9,61 @@ abstract class Product {
         print $this->getValue() . "\n";
     }
 }
+
+
+abstract class Product{
+    /**
+     * @var $id int - идентификатор продукта
+     * @var $name string - Название продукта
+     * @var $img array - Адреса изображений
+     * @var $price int - Цена продажи
+     * @var $description string - Описание продукта
+     * @var $keywords array - Ключевые слова
+     */
+    public $id;
+    public $name;
+    public $img;
+    public $price;
+    public $description;
+    public $keywords;
+
+    /**
+     * Product constructor.
+     * @param $id
+     * @param $name
+     * @param $img
+     * @param $price
+     * @param $description
+     * @param $keywords
+     */
+    public function __construct($id, $name, $img, $price, $description, $keywords){
+        $this->id = $id;
+        $this->name = $name;
+        $this->img = $img;
+        $this->price = $price;
+        $this->description = $description;
+        $this->keywords = $keywords;
+    }
+
+    abstract protected function showProduct();
+
+    public function view(){
+
+        $this->showProduct();
+       /* echo "<h1>$this->name</h1>";
+        echo "Изображения: ";
+        foreach ($this->img as $img){
+            echo "<p>$img</p>";
+        }
+        echo "<p>Цена: $this->price</p>";
+        echo "<p>Описание товара: $this->description</p>";
+        echo "Ключевые слова: ";
+        foreach ($this->keywords as $aKeyword){
+            echo "<p>$aKeyword</p>";
+        }*/
+    }
+
+}
 class ChildClass extends MyAbstractClass {
 
     protected function getValue() {
